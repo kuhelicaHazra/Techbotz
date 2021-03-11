37130f7d6037f3f5a0b9aef470d8a79173b76bc2
kuhelicaHazra <kuhelica@gmail.com>
2021-03-11 23:04:16 +0530

    developed for teckbotz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule {
-
-
  }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer, Input } from '@angular/core';
+import { $ } from '../../node_modules/protractor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '(window:scroll)': 'onScroll($event)'
+}
 })
 export class AppComponent {
   isCollapsed = false;
+  isScrolled = false;
+currPos: Number = 0;
+startPos: Number = 0;
+changePos: Number = 100;
+constructor(private el: ElementRef, private renderer: Renderer) {
+}
+
+onScroll(evt) {
+    /**
+     * BAD BAD way to do this
+     * window object can be wrapped as a service
+     * and then be imported here
+    */
+    this.currPos = (window.pageYOffset || evt.target.scrollTop)  - (evt.target.clientTop || 0);
+    if (this.currPos >= this.changePos ) {
+        this.isScrolled = true;
+    } else {
+        this.isScrolled = false;
+    }
+}
+
+initStateSave(evt) {
+    // trigger an event so child components can listen and save their states
+}
   onActivate(event) {
     const scrollToTop = window.setInterval(() => {
         const pos = window.pageYOffset;
@@ -16,4 +45,5 @@ export class AppComponent {
         }
     }, 16);
 }
+
 }
